030499f2662e09b51999eb3d96723740bd05c57d
FortuneNzobigeza <nzobigezaf@gmail.com>
2026-02-06 05:32:47 +0000

    quick folder creation, renaming files, added anew note to READ.md in A2

---
+++ A2_M1/scheduling.c
@@ -0,0 +1,9 @@
+/* 
+------------------------------------
+Student Name: Fortune Nzobigeza
+Student ID: 3131442
+Submission Date: February 21st 2026
+File Name: scheduling.c
+------------------------------------
+*/
+
